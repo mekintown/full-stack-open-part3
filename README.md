d2318ee07a640ae986a5055a3ec3a9cfc2c9fde0
Maekin <74455990+mekintown@users.noreply.github.com>
2023-07-05 17:13:07 +0700

    Finish 3.11 phonebook full stack

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # full-stack-open-part3
+
+https://silent-haze-5202.fly.dev/api/persons
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 
 app.use(express.json());
 
+app.use(express.static("build"));
+
 morgan.token("custom", function (req, res) {
     return JSON.stringify(req.body);
 });
@@ -87,7 +89,7 @@ app.post("/api/persons", (request, response) => {
     response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
