da3b2876ba9de5a98a7f28dc098dc5e0cbfac34c
Maekin <74455990+mekintown@users.noreply.github.com>
2023-07-08 12:40:02 +0700

    Finish 3.19*: Phonebook database, step7

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.delete("/api/persons/:id", (request, response) => {
         .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body;
 
     if (!body.name || !body.number) {
@@ -64,30 +64,33 @@ app.post("/api/persons", (request, response) => {
             error: "content missing",
         });
     }
-    // if (Person.find((currentPerson) => currentPerson.name === person.name)) {
-    //     return response.status(400).json({
-    //         error: "name must be unique",
-    //     });
-    // }
+
     const person = new Person({
         name: body.name,
         number: Number(body.number),
     });
 
-    person.save().then((savedPerson) => {
-        response.json(savedPerson);
-    });
+    person
+        .save()
+        .then((savedPerson) => {
+            response.json(savedPerson);
+        })
+        .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
     const body = request.body;
 
     const person = {
-        name: body.person,
+        name: body.name,
         number: body.number,
     };
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id, person, {
+        new: true,
+        runValidators: true,
+        context: "query",
+    })
         .then((updatedPerson) => {
             response.json(updatedPerson);
         })
@@ -103,8 +106,10 @@ app.use(unknownEndpoint);
 const errorHandler = (error, request, response, next) => {
     console.log(error);
 
-    if ((error.name = "Cast Error")) {
+    if (error.name === "Cast Error") {
         return response.status(400).send({ error: "malformatted id" });
+    } else if (error.name === "ValidationError") {
+        return response.status(400).json({ error: error.message });
     }
 
     next(error);
